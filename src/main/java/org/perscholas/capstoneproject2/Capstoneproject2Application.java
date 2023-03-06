d14b6e233faf2ddb1de072d413ffe8154f851b3e
Quang Le <quangle416@gmail.com>
2023-03-06 14:11:12 -0600

    First commit. Base Project, gitignore, and application properties done.

---
+++ src/main/java/org/perscholas/capstoneproject2/Capstoneproject2Application.java
@@ -0,0 +1,13 @@
+package org.perscholas.capstoneproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstoneproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Capstoneproject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/perscholas/capstoneproject2/Capstoneproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.capstoneproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstoneproject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
